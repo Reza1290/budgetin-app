ec21a40837bdd6bc662a518edf2c1ac6fc09183e
Reza1290 <reza.muktasib@gmail.com>
2024-04-13 13:35:01 +0700

    release: fix bar title

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
+      
         home: BottomNavbar(),
         theme: ThemeData(
           primarySwatch: Colors.blue,
