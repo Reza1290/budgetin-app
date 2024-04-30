486fd83619ca112dff07fe7ba84032ca9649c61d
Arsyita20 <arsyitadevanaya@gmail.com>
2024-05-01 00:56:30 +0700

    filter tapi warna belum sesuai

--- lib/widgets/modal/modal_filter.dart
+++ lib/widgets/modal/modal_filter.dart
@@ -1,7 +1,9 @@
 import 'package:budgetin/widgets/modal/budgetin_modal.dart';
+import 'package:budgetin/widgets/reusable/calendar.dart';
 import 'package:budgetin/widgets/reusable/title_modal.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FilterDialog extends StatefulWidget {
   @override
@@ -10,15 +12,15 @@ class FilterDialog extends StatefulWidget {
 
 class _FilterDialogState extends State<FilterDialog> {
   final List<String> items = [
-  'A_Item1',
-  'A_Item2',
-  'A_Item3',
-  'A_Item4',
-  'B_Item1',
-  'B_Item2',
-  'B_Item3',
-  'B_Item4',
-];
+    'A_Item1',
+    'A_Item2',
+    'A_Item3',
+    'A_Item4',
+    'B_Item1',
+    'B_Item2',
+    'B_Item3',
+    'B_Item4',
+  ];
 
   String? selectedValue;
   late DateTime selectedDate;
@@ -52,85 +54,144 @@ class _FilterDialogState extends State<FilterDialog> {
             ),
             const SizedBox(height: 6.0),
             DropdownButtonHideUnderline(
-        child: DropdownButton2<String>(
-          isExpanded: true,
-          hint: Text(
-            'Select Item',
-            style: TextStyle(
-              fontSize: 14,
-              color: Theme.of(context).hintColor,
-            ),
-          ),
-          items: items
-              .map((item) => DropdownMenuItem(
-                    value: item,
-                    child: Text(
-                      item,
-                      style: const TextStyle(
-                        fontSize: 14,
+              child: DropdownButton2<String>(
+                isExpanded: true,
+                hint: Text(
+                  'Select Item',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Theme.of(context).hintColor,
+                  ),
+                ),
+
+                items: items
+                    .map((item) => DropdownMenuItem(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: const TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+                value: selectedValue,
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value;
+                  });
+                },
+                buttonStyleData: ButtonStyleData(
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(
+                        color: Colors.black26,
+                      ),
+                      
+                    )),
+                dropdownStyleData: const DropdownStyleData(
+                  maxHeight: 200,
+                ),
+                menuItemStyleData: const MenuItemStyleData(
+                  height: 40,
+                ),
+                dropdownSearchData: DropdownSearchData(
+                  searchController: textEditingController,
+                  searchInnerWidgetHeight: 50,
+                  searchInnerWidget: Container(
+                    height: 50,
+                    padding: const EdgeInsets.only(
+                      top: 8,
+                      bottom: 4,
+                      right: 8,
+                      left: 8,
+                    ),
+                    child: TextFormField(
+                      expands: true,
+                      maxLines: null,
+                      controller: textEditingController,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 8,
+                        ),
+                        hintText: 'Search for an item...',
+                        hintStyle: const TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                          // Menambahkan border
+                          borderRadius: BorderRadius.circular(
+                              6.0), // Mengatur sudut border
+                          borderSide: BorderSide(), // Mengatur sisi border
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          // Mengubah warna border ketika aktif menjadi biru
+                          borderRadius: BorderRadius.circular(
+                              6.0), // Mengatur sudut border
+                          borderSide: BorderSide(
+                              color:
+                                  Color(0xFF1D77FF)), // Mengatur warna border
+                        ),
                       ),
                     ),
-                  ))
-              .toList(),
-          value: selectedValue,
-          onChanged: (value) {
-            setState(() {
-              selectedValue = value;
-            });
-          },
-          buttonStyleData: const ButtonStyleData(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            height: 40,
-            width: 200,
-          ),
-          dropdownStyleData: const DropdownStyleData(
-            maxHeight: 200,
-          ),
-          menuItemStyleData: const MenuItemStyleData(
-            height: 40,
-          ),
-          dropdownSearchData: DropdownSearchData(
-            searchController: textEditingController,
-            searchInnerWidgetHeight: 50,
-            searchInnerWidget: Container(
-              height: 50,
-              padding: const EdgeInsets.only(
-                top: 8,
-                bottom: 4,
-                right: 8,
-                left: 8,
-              ),
-              child: TextFormField(
-                expands: true,
-                maxLines: null,
-                controller: textEditingController,
-                decoration: InputDecoration(
-                  isDense: true,
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 8,
                   ),
-                  hintText: 'Search for an item...',
-                  hintStyle: const TextStyle(fontSize: 12),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
+                  searchMatchFn: (item, searchValue) {
+                    return item.value.toString().contains(searchValue);
+                  },
+                ),
+                //This to clear the search value when you close the menu
+                onMenuStateChange: (isOpen) {
+                  if (!isOpen) {
+                    textEditingController.clear();
+                  }
+                },
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            const Text(
+              "Tanggal",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 6.0),
+            SizedBox(
+              height: 40,
+              child: GestureDetector(
+                onTap: () async {
+                  final DateTime? pickedDate =
+                      await selectDate(context, selectedDate);
+                  if (pickedDate != null && pickedDate != selectedDate) {
+                    setState(() {
+                      selectedDate = pickedDate;
+                    });
+                  }
+                },
+                child: AbsorbPointer(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                      ),
+                      suffixIcon: const Icon(Icons.calendar_month),
+                      hintText:
+                          "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}",
+                      hintStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 15),
+                    ),
+                    readOnly: true,
                   ),
                 ),
               ),
             ),
-            searchMatchFn: (item, searchValue) {
-              return item.value.toString().contains(searchValue);
-            },
-          ),
-          //This to clear the search value when you close the menu
-          onMenuStateChange: (isOpen) {
-            if (!isOpen) {
-              textEditingController.clear();
-            }
-          },
-        ),
-      ),
-            // Your Calendar selection code here
           ],
         ),
       ),
