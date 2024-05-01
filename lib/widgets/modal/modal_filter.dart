25a28218cbcb2f1ed769b498dfd765b63c1d5491
Arsyita20 <arsyitadevanaya@gmail.com>
2024-05-01 10:40:48 +0700

    edit filter transaksi

--- lib/widgets/modal/modal_filter.dart
+++ lib/widgets/modal/modal_filter.dart
@@ -1,9 +1,11 @@
 import 'package:budgetin/widgets/modal/budgetin_modal.dart';
-import 'package:budgetin/widgets/reusable/calendar.dart';
 import 'package:budgetin/widgets/reusable/title_modal.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+final today = DateUtils.dateOnly(DateTime.now());
 
 class FilterDialog extends StatefulWidget {
   @override
@@ -86,9 +88,8 @@ class _FilterDialogState extends State<FilterDialog> {
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(6),
                       border: Border.all(
-                        color: Color(0xFFD1D1D1), 
+                        color: Color(0xFFD1D1D1),
                       ),
-                      
                     )),
                 dropdownStyleData: const DropdownStyleData(
                   maxHeight: 200,
@@ -123,7 +124,8 @@ class _FilterDialogState extends State<FilterDialog> {
                           // Menambahkan border
                           borderRadius: BorderRadius.circular(
                               6.0), // Mengatur sudut border
-                          borderSide: BorderSide(color: Color(0xFFD1D1D1)), // Mengatur sisi border
+                          borderSide: BorderSide(
+                              color: Color(0xFFD1D1D1)), // Mengatur sisi border
                         ),
                         focusedBorder: OutlineInputBorder(
                           // Mengubah warna border ketika aktif menjadi biru
@@ -154,43 +156,7 @@ class _FilterDialogState extends State<FilterDialog> {
               style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
             ),
             const SizedBox(height: 6.0),
-            SizedBox(
-              height: 40,
-              child: GestureDetector(
-                onTap: () async {
-                  final DateTime? pickedDate =
-                      await selectDate(context, selectedDate);
-                  if (pickedDate != null && pickedDate != selectedDate) {
-                    setState(() {
-                      selectedDate = pickedDate;
-                    });
-                  }
-                },
-                child: AbsorbPointer(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(6.0),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0),),
-                      ),
-                      suffixIcon: const Icon(Icons.calendar_month),
-                      hintText:
-                          "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}",
-                      hintStyle: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 15),
-                    ),
-                    readOnly: true,
-                  ),
-                ),
-              ),
-            ),
+            _buildCalendarDialogButton(),
           ],
         ),
       ),
@@ -199,55 +165,229 @@ class _FilterDialogState extends State<FilterDialog> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  selectedValue = null;
-                  selectedDate = DateTime.now();
-                });
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Color(0xffF2F2F2)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: BorderSide(color: Colors.transparent),
+            SizedBox(
+              width: 110,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    selectedValue = null;
+                    selectedDate = DateTime.now();
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(0xffF2F2F2)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    EdgeInsets.all(
+                        2), // Atur padding di sini sesuai kebutuhan Anda
                   ),
                 ),
-              ),
-              child: Text(
-                'Reset',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xffA3A3A3),
+                child: Text(
+                  'Reset',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xffA3A3A3),
+                  ),
                 ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Apply filter logic here
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF1D77FF)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: BorderSide(color: Colors.transparent),
+            SizedBox(
+              width: 110,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Apply filter logic here
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(0xFF1D77FF)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                      side: BorderSide(color: Colors.transparent),
+                    ),
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    EdgeInsets.all(
+                        2), // Atur padding di sini sesuai kebutuhan Anda
                   ),
                 ),
-              ),
-              child: Text(
-                'Terapkan',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
+                child: Text(
+                  'Terapkan',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  String _getValueText(
+    CalendarDatePicker2Type datePickerType,
+    DateTime value,
+  ) {
+    var date = DateUtils.dateOnly(value); // Menghilangkan waktu dari DateTime
+    var valueText = date
+        .toString()
+        .replaceAll('00:00:00.000', ''); // Menghilangkan informasi waktu
+    return valueText;
+  }
+
+  _buildCalendarDialogButton() {
+    const dayTextStyle =
+        TextStyle(color: Colors.black, fontWeight: FontWeight.w700);
+
+    final config = CalendarDatePicker2WithActionButtonsConfig(
+      calendarViewScrollPhysics: const NeverScrollableScrollPhysics(),
+      dayTextStyle: dayTextStyle,
+      calendarType: CalendarDatePicker2Type.single,
+      selectedDayHighlightColor: Color(0xFF1D77FF),
+      closeDialogOnCancelTapped: true,
+      firstDayOfWeek: 1,
+      weekdayLabelTextStyle: const TextStyle(
+        color: Colors.black87,
+        fontWeight: FontWeight.bold,
+      ),
+      controlsTextStyle: const TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontWeight: FontWeight.bold,
+      ),
+      selectedDayTextStyle: dayTextStyle.copyWith(color: Colors.white),
+      dayBuilder: ({
+        required date,
+        textStyle,
+        decoration,
+        isSelected,
+        isDisabled,
+        isToday,
+      }) {
+        Widget? dayWidget;
+        if (date.day % 3 == 0 && date.day % 9 != 0) {
+          dayWidget = Container(
+            decoration: decoration,
+            child: Center(
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Text(
+                    MaterialLocalizations.of(context).formatDecimal(date.day),
+                    style: textStyle,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 27.5),
+                    child: Container(
+                      height: 4,
+                      width: 4,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: isSelected == true
+                            ? Colors.white
+                            : Colors.grey[500],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        return dayWidget;
+      },
+      yearBuilder: ({
+        required year,
+        decoration,
+        isCurrentYear,
+        isDisabled,
+        isSelected,
+        textStyle,
+      }) {
+        return Center(
+          child: Container(
+            decoration: decoration,
+            height: 36,
+            width: 72,
+            child: Center(
+              child: Semantics(
+                selected: isSelected,
+                button: true,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      year.toString(),
+                      style: textStyle,
+                    ),
+                    if (isCurrentYear == true)
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        margin: const EdgeInsets.only(left: 5),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    return SizedBox(
+      height: 40,
+      child: GestureDetector(
+        onTap: () async {
+          final values = await showCalendarDatePicker2Dialog(
+            context: context,
+            config: config,
+            dialogSize: const Size(325, 400),
+            borderRadius: BorderRadius.circular(15),
+            value: [selectedDate],
+            dialogBackgroundColor: Colors.white,
+          );
+          if (values != null) {
+            setState(() {
+              selectedDate = values[0] ??
+                  DateTime.now(); // Tetapkan nilai yang dipilih ke selectedDate
+            });
+          }
+        },
+        child: AbsorbPointer(
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(6.0),
+                ),
+              ),
+              suffixIcon: const Icon(Icons.calendar_month),
+              hintText:
+                  "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}",
+              hintStyle: const TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+            ),
+            readOnly: true,
+          ),
         ),
       ),
     );
