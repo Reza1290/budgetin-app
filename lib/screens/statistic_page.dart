ab7df5d520b857e8b8bf4e49f21a6f4f99ff33c7
Reza1290 <reza.muktasib@gmail.com>
2024-06-06 22:15:31 +0700

    fix:conflict

--- lib/screens/statistic_page.dart
+++ lib/screens/statistic_page.dart
@@ -1,3 +1,4 @@
+import 'package:budgetin/main.dart';
 import 'package:budgetin/utilities/them.dart';
 import 'package:budgetin/widgets/statistic/statistic_bar.dart';
 import 'package:budgetin/widgets/statistic/statistic_circular.dart';
@@ -8,6 +9,28 @@ class StatisticPage extends StatefulWidget {
 
   @override
   State<StatisticPage> createState() => _StatisticPageState();
+}
+
+const List<String> financialAdvices = [
+  'Pengeluaranmu sangat rendah bulan lalu. Pertahankan kebiasaan baik ini dan terus tabung lebih banyak!',
+  'Pengeluaranmu cukup rendah. Bagus! Usahakan untuk menyimpan sebagian dari tabungan untuk investasi.',
+  'Pengeluaranmu sedang. Cobalah untuk mengidentifikasi area di mana kamu bisa mengurangi pengeluaran.',
+  'Pengeluaranmu agak tinggi bulan lalu. Pertimbangkan untuk mengurangi pengeluaran yang tidak perlu.',
+  'Pengeluaranmu sangat tinggi. Disarankan untuk membuat anggaran dan meninjau kembali pengeluaran rutin.',
+];
+
+String getAdvice(double persen) {
+  if (persen >= 90) {
+    return financialAdvices[4];
+  } else if (persen >= 80) {
+    return financialAdvices[3];
+  } else if (persen >= 70) {
+    return financialAdvices[2];
+  } else if (persen >= 60) {
+    return financialAdvices[1];
+  } else {
+    return financialAdvices[0];
+  }
 }
 
 class _StatisticPageState extends State<StatisticPage> {
@@ -59,7 +82,47 @@ class _StatisticPageState extends State<StatisticPage> {
                   child: StatisticCircular(
                     bulan: _selectedIndex,
                   ),
-                )
+                ),
+                FutureBuilder(
+                    future: db!.prsentaseExpense(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasData) {
+                        double persen = snapshot.data!;
+                        return Container(
+                          padding: EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue[100], // Warna latar belakang
+                            borderRadius: BorderRadius.circular(
+                                10.0), // Bentuk rounded corners
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey
+                                    .withOpacity(0.5), // Warna shadow
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3), // Offset dari shadow
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              getAdvice(persen),
+                              style: TextStyle(
+                                fontSize: 18.0, // Ukuran font
+                                fontWeight: FontWeight.bold, // Ketebalan font
+                                color: Colors.black, // Warna font
+                              ),
+                              textAlign:
+                                  TextAlign.center, // Posisi teks di tengah
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Text('');
+                      }
+                    })
               ],
             ),
           ),
